67b4a493b6dfd9ded05002a503913c2535e6ab39
Jan Herlyn <jan.herlyn@gmail.com>
2016-07-14 16:05:45 +0200

    fixed a bug in the feature that caused the generation of nonsential data

--- libanalyze/src/root_mean_square_feature.c
+++ libanalyze/src/root_mean_square_feature.c
@@ -10,11 +10,13 @@ extern "C" {
 
 float root_mean_square(float voltage[], float current[], int buffer_size, int offset)
 {
+    
     float result = 0;
     int byte_counter = 0;
     for (; byte_counter < DATA_POINTS_PER_WAVE_LENGTH; ++ byte_counter) {
         result += current[(byte_counter + offset) % buffer_size] * current[(byte_counter + offset) % buffer_size];
     }
+    
     return sqrt(result / DATA_POINTS_PER_WAVE_LENGTH);
 }
 
@@ -26,13 +28,12 @@ int root_mean_square_feature(RootMeanSquareFeature* feature, float voltage[], fl
         feature->data[msq_counter] = 0;
     }
 
-
     msq_counter = 0;
 
     int bytes_counter = 0;
     while (bytes_counter < DATA_POINTS_PER_FEATURE - DATA_POINTS_PER_WAVE_LENGTH) {
 
-        feature->data[msq_counter] = root_mean_square(voltage, current, buffer_size, bytes_counter);
+        feature->data[msq_counter] = root_mean_square(voltage, current, buffer_size, bytes_counter + offset);
         bytes_counter += DATA_POINTS_PER_WAVE_LENGTH;
         ++msq_counter;
 
